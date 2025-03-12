241fb8846572731e1a2716735f7627644e0d2bfe
[B[B[B[B[B[B[B[B[B[B[B[B <tomwparkinson@hotmail.co.uk>
2025-03-12 16:35:23 +0000

    sorted seeding issues

--- app/models/hexagon.rb
+++ app/models/hexagon.rb
@@ -3,7 +3,7 @@ class Hexagon < ApplicationRecord
   has_many :filters
   belongs_to :hex_grid
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation :geocode, if: :will_save_change_to_address?
 
   validates :lat, :lon, presence: true
 end
