18a8e4fe417f01332435cd0d8f12cbfd1234bd16
Ed Audland <edaudland@gmail.com>
2025-03-13 09:04:34 +0000

    Created singular filters object (of all POI data) to pass through

--- app/controllers/hex_grids_controller.rb
+++ app/controllers/hex_grids_controller.rb
@@ -6,8 +6,14 @@ class HexGridsController < ApplicationController
     @pubs = Poi.where(category: "pub")
     @restaurants = Poi.where(category: "restaurant")
     @stations = Poi.where(category: "station")
-    
-    
+
+    categories = Poi.distinct.pluck(:category).sort
+
+    @filters = categories.each_with_object({}) do |category, hash|
+      hash[category] = Poi.where(category: category)
+    end
+
+
     address = params[:address].gsub(/[^a-zA-Z0-9\s]/, '').gsub(" ", "%20")
     url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{address}.json?access_token=#{ENV['MAPBOX_API_KEY']}"
     uri = URI.parse(url)
