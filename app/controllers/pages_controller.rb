35c2bdd02e53b879d4e55f1ba8cd25ba63f3b6fc
Charlie Hartigan <charliehartigan@icloud.com>
2025-03-19 12:58:47 +0000

    Checkbox Style and HTML slight reshuffle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,6 @@ class PagesController < ApplicationController
   end
 
   def map
-
     @hexagons = Hexagon.all
     @params =  params[:address]
     # Retrieve distinct categories from the POI database and create a Filters hash to pass to HTML & JS
