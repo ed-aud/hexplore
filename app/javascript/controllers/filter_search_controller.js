7a505dd7b61284c8c296e46c43ffdc2859c01c8d
Ed Audland <edaudland@gmail.com>
2025-03-19 14:13:27 +0000

    Updated search toggles

--- app/javascript/controllers/filter_search_controller.js
+++ app/javascript/controllers/filter_search_controller.js
@@ -9,18 +9,20 @@ export default class extends Controller {
 
     this.categoryTargets.forEach(category => {
       let categoryLetters = category.textContent.toLowerCase()
-      category.parentNode.style.display = categoryLetters.includes(searchQuery) ? "block" : "none";
+      category.parentNode.style.display = categoryLetters.includes(searchQuery) ? "" : "none";
     });
   }
 
   // Function to clear search bar and display all categories again once a category has been selected
   clearSearch() {
     this.searchTermTarget.value = ""
+
     this.categoryTargets.forEach(category => {
-      category.style.display = "block"
-    })
+      category.parentNode.style.display = ""
+    });
   }
 
+  // Function to deselect all selected categories
   deselectAll() {
     this.checkboxTargets.forEach(checkbox => {
       checkbox.checked = false
